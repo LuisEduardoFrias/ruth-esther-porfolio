62751d9c2b4d7c223d2e57d88b187de3556e092d
luis eduardo <luiseduardofrias27@gmail.com>
2025-06-28 09:38:24 -0400

    trabajando en el ejercicio de Análisis de Ventas Regionales

--- src/consts/workExperiences.ts
+++ src/consts/workExperiences.ts
@@ -1,21 +1,21 @@
 import { workExperience } from '@/types/workExperience'
 
 export const WORKEXPERIENCES: workExperience[] = [
-  {
-    company: 'Cámara de cuentas.',
-    cargo: 'Analista',
-    area: 'Departamento tegnoligia',
-    tasks: ['Testing', 'Documentacion', 'Levantamiento de requeriemiento'],
-  },
-  {
-    company: 'Cámara de cuentas.',
-    cargo: 'Secretaria',
-    area: 'Departamento de comunicación',
-    tasks: [],
-  },
-  {
-    company: 'Ministerio de Interior y Policía',
-    cargo: 'Oficial de protocolo',
-    tasks: []
-  },
+   {
+      company: 'La cámara de cuentas.',
+      cargo: 'Analista',
+      area: 'Departamento tecnoligia',
+      tasks: ['Testing', 'Documentacion', 'Levantamiento de requeriemiento'],
+   },
+   {
+      company: 'Cámara de cuentas.',
+      cargo: 'Secretaria',
+      area: 'Departamento de comunicación',
+      tasks: [],
+   },
+   {
+      company: 'El Ministerio de Interior y Policía',
+      cargo: 'Oficial de protocolo',
+      tasks: []
+   },
 ];
